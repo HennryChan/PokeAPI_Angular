da8c165a5c5ea7b75356448e6c14bb43ce3915d5
Hennry Chan <dual.hennry.chan@gmail.com>
2022-12-20 22:46:12 -0600

    Se hadilito todos los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,32 +6,32 @@ import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//import { contadorReducer } from './contador/contador.reducer';
-//import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-//import { HijoComponent } from './contador/hijo/hijo.component';
-//import { NietoComponent } from './contador/nieto/nieto.component';
+import { contadorReducer } from './contador/contador.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
 import { CardComponent } from './pokemon/card/card.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    //AppComponent,
-    CardComponent
-    //HijoComponent,
-   //NietoComponent
+    AppComponent,
+    CardComponent,
+    HijoComponent,
+   NietoComponent
 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
-    //AppRoutingModule
-    //StoreModule.forRoot({count: contadorReducer}),
-   // StoreDevtoolsModule.instrument({
-     // maxAge: 25,
-     // logOnly: environment.production,
-   // }),
+    FormsModule,
+    AppRoutingModule,
+    StoreModule.forRoot({count: contadorReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
